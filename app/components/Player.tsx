1a7d788791948a81ee2a4fb0910ad1834bad707b
daspharion <daspharion@gmail.com>
2020-08-29 07:01:35 +0300

    better queue/interface

--- app/components/Player.tsx
+++ app/components/Player.tsx
@@ -42,7 +42,7 @@ export const Player = (props: Props) => {
 
   return (
     <div className='player'>
-      <span>PLAYER</span>
+      <span>NOW PLAYING</span>
       {rank === 0 ?
         <YouTube videoId={videoId || 'lep7-tH15MY'} containerClassName='youtube-player'
           onReady={({ target }) => { setPlayer(target); if (isPlaying) target.playVideo() }}
@@ -51,14 +51,14 @@ export const Player = (props: Props) => {
           onEnd={queue.length ? next : () => playerRef.current.playVideo()}
           opts={{
             host: 'https://www.youtube-nocookie.com',
-            playerVars: { hl: 'en', rel: 0, autoplay: 1, modestbranding: 1, origin: location.origin, ...!videoId && { controls: 0 } }
+            playerVars: { hl: 'en', rel: 0, autoplay: 0, modestbranding: 1, origin: location.origin, ...!videoId && { controls: 0 } }
           }}
         /> :
         <>
-          <div className='player-wrapper'>
+          <div className='wrapper'>
             <img src={`https://i.ytimg.com/vi/${videoId || 'lep7-tH15MY'}/${thumb}.jpg`} />
           </div>
-          <span>{title}</span>
+          <span className='title'>{title}</span>
           <div className='controls'>
             <img src={'images/icon-next.svg'} onClick={prev} />
             <img src={`images/icon-${isPlaying ? 'pause' : 'play'}.svg`} onClick={() => request(`toggle?q=${+!isPlaying}`)} />
--- app/components/Queue.tsx
+++ app/components/Queue.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useRef } from 'react'
 import { List } from 'react-movable'
 import { request } from '../utils'
-import { TQueue } from '../typings'
+import { TQueue, TQueueSearch } from '../typings'
 
 import './styles/queue.scss'
 
@@ -11,10 +11,10 @@ type Props = {
 }
 
 const wait = (n: number) => new Promise((resolve) => setTimeout(resolve, n))
-const formatTime = (t: number) => [~~(t / 60), t % 60].map((n) => `${n}`.padStart(2, '0')).join(':')
+const format = (t: number) => [~~(t / 60), t % 60].map((n) => `${n}`.padStart(2, '0')).join(':')
 
 export const Queue = ({ queue, queueId }: Props) => {
-  const [search, setSearch] = useState<TQueue[]>(null)
+  const [search, setSearch] = useState<TQueueSearch[]>(null)
   const [container, setContainer] = useState<Element>(null)
   const [fetching, setFetching] = useState<boolean>(false)
   const [query, setQuery] = useState<string>('')
@@ -29,17 +29,27 @@ export const Queue = ({ queue, queueId }: Props) => {
   }
 
   const getQueue = async (query: string, page?: string) => {
+    if (query.length < 3) return
+
     setFetching(true)
     const res = await request(`search?query=${query}${page ? `&page=${page}` : ''}`)
     setFetching(false)
     return res
   }
 
+  const delQueue = (id: number) => {
+    request(`queue?id=${id}`, { method: 'DELETE' })
+    if (queueId === id && queue.length > 1) {
+      const i = queue.findIndex(({ id }) => id === queueId) + 1
+      request(`play?id=${i < queue.length ? queue[i].id : queue[0].id}`, { method: 'PATCH' })
+    }
+  }
+
   const onQuery = async (e: React.ChangeEvent<HTMLInputElement>) => {
     setQuery(e.target.value)
 
     const { value: query } = input.current
-    const check = async (data: TQueue[]) => input.current.value === query ? data : Promise.reject()
+    const check = async (data: TQueueSearch[]) => input.current.value === query ? data : Promise.reject()
 
     if (query.length < 2) {
       setSearch(null)
@@ -57,53 +67,65 @@ export const Queue = ({ queue, queueId }: Props) => {
   }
 
   return (
-    <div className='queue'>
+    <div className='queue' ref={list}>
       <span>{search ? 'SEARCH' : 'QUEUE'}</span>
 
-      <div className={`fetch${fetching ? '' : ' hidden'}`} />
-
-      <div className='queue-list' ref={list}>
-        {!search ?
-          <List
-            values={queue}
-            lockVertically={true}
-            container={container}
-            onChange={({ oldIndex, newIndex }) => {
-              request(`queue?id=${queue[oldIndex].id}&pos=${queue[newIndex].position}`, { method: 'PATCH' })
-            }}
-            renderList={({ children, props }) => <div className='queue-list-list' {...props}>{children}</div>}
-            renderItem={({ value: { id, videoId, title, channel, duration }, props }) => (
-              <div className={'queue-song'} {...props} key={id}>
-                {id === queueId && <div className='queue-playing' /> }
-                <img src={`https://i.ytimg.com/vi/${videoId}/mqdefault.jpg`}
-                  loading='lazy' width='100px' height='56px'
-                  onClick={() => request(`play?id=${id}`, { method: 'PATCH' }) }
-                />
-                <div className='queue-song-info' data-movable-handle>
-                  <span>{title}</span>
-                  <span>{channel}</span>
-                </div>
-                <div className='queue-song-right'>
-                  {!search && <img className='queue-remove' onClick={() => request(`queue?id=${id}`, { method: 'DELETE' })} />}
-                  <span>{formatTime(duration)}</span>
-                </div>
+      {!search ? queue.length ?
+        <List
+          values={queue}
+          lockVertically={true}
+          container={container}
+          onChange={({ oldIndex, newIndex }) => {
+            request(`queue?id=${queue[oldIndex].id}&pos=${queue[newIndex].position}`, { method: 'PATCH' })
+          }}
+          renderList={({ children, props }) => <div className='list' {...props}>{children}</div>}
+          renderItem={({ value: { id, videoId, title, channel, duration }, props, isDragged }) => (
+            <div className={`song${isDragged ? ' dragged' : ''}${id === queueId ? ' invert' : ''}`} {...props} key={id}>
+              <div className='thumbnail' onClick={() => request(`play?id=${id}`, { method: 'PATCH' }) }>
+                <img src={`https://i.ytimg.com/vi/${videoId}/mqdefault.jpg`} loading='lazy' width='100px' height='56px' />
+                <img src='images/icon-start.svg' width='40px' height='40px' />
               </div>
-            )}
-          /> :
-          <div className='queue-list-list'> {
-            search.map(({ id, title, channel, duration }) =>
-              <div className={'queue-song'} key={id} onClick={() => request(`queue?id=${id}`, { method: 'PUT' })}>
+              <div className='info'>
+                <span>{title}</span>
+                <span>{channel}</span>
+              </div>
+              <div className='move' data-movable-handle>
+                <img src='images/icon-move.svg' width='4px' height='48px' />
+              </div>
+              <div className='right'>
+                <img src='images/icon-remove.svg' width='24px' height='24px' onClick={() => delQueue(id)} />
+                <span>{format(duration)}</span>
+              </div>
+            </div>
+          )}
+        /> :
+        <div className='list'>
+          <div className='empty'>
+            <img src='images/icon-emptyqueue.svg' width='256x' height='256px' />
+            <span>it's lonely out here...</span>
+          </div>
+        </div> :
+        <div className='list'> {
+          search.map(({ id, title, channel, duration, uploaded }) =>
+            <div className={`song search${queue.find((q) => q.videoId === id) ? ' added' : ''}`} key={id} onClick={() => request(`queue?id=${id}`, { method: 'PUT' })}>
+              <div className='thumbnail' onClick={() => request(`play?id=${id}`, { method: 'PATCH' }) }>
                 <img src={`https://i.ytimg.com/vi/${id}/mqdefault.jpg`} loading='lazy' width='100px' height='56px' />
-                <div className='queue-song-info'>
-                  <span>{title}</span>
-                  <span>{channel}</span>
-                </div>
-                <div className='queue-song-right'>
-                  <span>{formatTime(duration)}</span>
-                </div>
+                <img src='images/icon-add.svg' width='40px' height='40px' />
               </div>
-            )}</div>}
-      </div>
+              <div className='info'>
+                <span>{title}</span>
+                <span>{channel}</span>
+              </div>
+              <div className='right'>
+                <span>{format(duration)}</span>
+                <span>{new Date(uploaded).toLocaleDateString().replace(/\/(?=\d{3})/, '\n')}</span>
+              </div>
+            </div>
+          )}
+        </div>
+      }
+
+      <div className={`fetch${fetching ? '' : ' hidden'}`} />
 
       <div className='queue-add'>
         <input
